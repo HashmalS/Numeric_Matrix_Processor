6cfe41437e3d342718e1982bb47ac7ef0017f522
Sergey Soroka <dragoon8370@gmail.com>
2021-08-28 14:14:58 +0300

    Stage 3/6 Completed

--- Numeric Matrix Processor/task/src/processor/Main.java
+++ Numeric Matrix Processor/task/src/processor/Main.java
@@ -1,81 +1,101 @@
 package processor;
 
-import javax.annotation.processing.SupportedSourceVersion;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    final static Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        /*
-        int m1 = scanner.nextInt();
-        int n1 = scanner.nextInt();
-        int[][] matrix1 = new int[m1][n1];
-        for (int i = 0; i < m1; i++) {
-            for (int j = 0; j < n1; j++) {
-                matrix1[i][j] = scanner.nextInt();
-            }
-        }
-
-        int m2 = scanner.nextInt();
-        int n2 = scanner.nextInt();
-        int[][] matrix2 = new int[m2][n2];
-        for (int i = 0; i < m2; i++) {
-            for (int j = 0; j < n2; j++) {
-                matrix2[i][j] = scanner.nextInt();
-            }
-        }
-
-        try {
-            int[][] sum = add(matrix1, matrix2);
-            for (int i = 0; i < m1; i++) {
-                for (int j = 0; j < n1; j++) {
-                    System.out.printf("%d ", sum[i][j]);
+        int currentCommand = 0;
+        double[][] result;
+        do {
+            try {
+                currentCommand = displayMenu();
+                switch (currentCommand) {
+                    case 1:
+                        result = add();
+                        displayResult(result);
+                        break;
+                    case 2:
+                        result = multiplyByNumber();
+                        displayResult(result);
+                        break;
+                    case 3:
+                        result = multiplyMatrices();
+                        displayResult(result);
+                        break;
+                    default:
+                        break;
                 }
-                System.out.println();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-         */
-
-        int m1 = scanner.nextInt();
-        int n1 = scanner.nextInt();
-        int[][] matrix1 = new int[m1][n1];
-        for (int i = 0; i < m1; i++) {
-            for (int j = 0; j < n1; j++) {
-                matrix1[i][j] = scanner.nextInt();
-            }
-        }
-        int multiplier = scanner.nextInt();
-
-        int[][] multipliedMatrix = multiplyByNumber(matrix1, multiplier);
-        for (int i = 0; i < m1; i++) {
-            for (int j = 0; j < n1; j++) {
-                System.out.printf("%d ", multipliedMatrix[i][j]);
-            }
-            System.out.println();
-        }
+        } while (currentCommand != 0);
     }
 
-    public static int[][] add(int[][] m1, int[][] m2) throws Exception {
-        if (m1.length != m2.length || m1[0].length != m2[0].length) {
-            throw new Exception("ERROR");
+    public static int displayMenu() throws Exception {
+        System.out.print("1. Add matrices\n2. Multiply matrix by a constant\n3. Multiply matrices\n0. Exit\n");
+        System.out.print("Your choice: ");
+        int command = scanner.nextInt();
+        if (command <0 || command > 3) {
+            throw new Exception("Error: no such command");
+        }
+        return command;
+    }
+
+    public static double[][] add() throws IllegalArgumentException {
+        System.out.print("Enter size of first matrix: ");
+        int m1 = scanner.nextInt();
+        int n1 = scanner.nextInt();
+        double[][] matrix1 = new double[m1][n1];
+        System.out.print("Enter first matrix:\n");
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                matrix1[i][j] = scanner.nextDouble();
+            }
         }
 
-        int[][] result = new int[m1.length][m1[0].length];
-        for (int i = 0; i < m1.length; i++) {
-            for (int j = 0; j < m1[0].length; j++) {
-                result[i][j] = m1[i][j] + m2[i][j];
+        System.out.print("Enter size of second matrix: ");
+        int m2 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        double[][] matrix2 = new double[m2][n2];
+        System.out.print("Enter second matrix:\n");
+        for (int i = 0; i < m2; i++) {
+            for (int j = 0; j < n2; j++) {
+                matrix2[i][j] = scanner.nextDouble();
+            }
+        }
+
+        if (matrix1.length != matrix2.length || matrix1[0].length != matrix2[0].length) {
+            throw new IllegalArgumentException("Error: Wrong dimensions.");
+        }
+
+        double[][] result = new double[matrix1.length][matrix1[0].length];
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1[0].length; j++) {
+                result[i][j] = matrix1[i][j] + matrix2[i][j];
             }
         }
 
         return result;
     }
 
-    public static int[][] multiplyByNumber(int[][] matrix, int multiplier) {
-        int[][] result = new int[matrix.length][matrix[0].length];
+    public static double[][] multiplyByNumber() {
+        System.out.print("Enter size of matrix: ");
+        int m1 = scanner.nextInt();
+        int n1 = scanner.nextInt();
+        double[][] matrix = new double[m1][n1];
+        System.out.print("Enter matrix:\n");
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                matrix[i][j] = scanner.nextDouble();
+            }
+        }
+        System.out.print("Enter constant: ");
+        double multiplier = scanner.nextDouble();
+
+        double[][] result = new double[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 result[i][j] = matrix[i][j] * multiplier;
@@ -83,5 +103,61 @@ public class Main {
         }
 
         return result;
+    }
+
+    public static double[][] multiplyMatrices() throws IllegalArgumentException {
+        System.out.print("Enter size of first matrix: ");
+        System.out.print("Enter size of first matrix: ");
+        int m1 = scanner.nextInt();
+        int n1 = scanner.nextInt();
+        double[][] matrix1 = new double[m1][n1];
+        System.out.print("Enter first matrix:\n");
+        for (int i = 0; i < m1; i++) {
+            for (int j = 0; j < n1; j++) {
+                matrix1[i][j] = scanner.nextDouble();
+            }
+        }
+
+        System.out.print("Enter size of second matrix: ");
+        int m2 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        double[][] matrix2 = new double[m2][n2];
+        System.out.print("Enter second matrix:\n");
+        for (int i = 0; i < m2; i++) {
+            for (int j = 0; j < n2; j++) {
+                matrix2[i][j] = scanner.nextDouble();
+            }
+        }
+
+        if (matrix1[0].length != matrix2.length) {
+            throw new IllegalArgumentException("Error: wrong matrix sizes");
+        }
+
+        double[][] result = new double[matrix1.length][matrix2[0].length];
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix2[0].length; j++) {
+                result[i][j] = 0;
+            }
+        }
+
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix2[0].length; j++) {
+                for (int k = 0; k < matrix1[0].length; k++) {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void displayResult(double[][] matrix) {
+        System.out.print("The result is:\n");
+        for (double[] doubles : matrix) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.printf("%f ", doubles[j]);
+            }
+            System.out.println();
+        }
     }
 }
